060f8ffdbb9367d3717202ee8426a0e8266ee327
Emilly <emillyhortapy@gmail.com>
2022-11-10 12:13:26 -0300

    add arquivos php

---
+++ action.php
@@ -0,0 +1,14 @@
+<table class="table">
+    <tbody> 
+        <tr class="cinza">
+            <td><?php echo $nome = $_GET['nome']; ?><br></td>
+            <td><?php echo $date = $_GET['date']; ?><br></td>
+            <td><?php echo $adress = $_GET['adress']; ?><br></td>
+            <td><?php echo $CEP = $_GET['CEP']; ?><br></td>
+            <td><?php echo $phone = $_GET['phone']; ?><br></td>
+        <?php echo "Cadastrado com sucesso!"?> 
+        </td>
+        </tr>
+    </tbody>
+</table>
+    
