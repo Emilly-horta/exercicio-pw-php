3fe0fcf5d890782384509a02d5fb9713687786d3
Emilly <emillyhortapy@gmail.com>
2022-11-14 20:45:50 -0300

    add index

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+
+<head></head>
+
+<body>
+    <form action="action.php" method="post">
+        <label>Nome</label>
+        <input type="text" name="nome" />
+
+        <label>birthday</label>
+        <input type="text" name="date" />
+
+        <label>Adress</label>
+        <input type="text" name="adress" />
+
+        <label>CEP</label>
+        <input type="text" name="CEP" />
+
+        <label>Phone</label>
+        <input type="phone" name="phone">
+
+        <p><input type="submit" value="cadastrar" /></p>
+
+    </form>
+</body>
+
+</html>
